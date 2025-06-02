4ac7a5b98d09d9dbc50679e609e8269e27df7ccd
Ajai-3 <ajaipjayan@gmail.com>
2025-06-02 14:12:00 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <h1>app</h1>
+    {/* <Routes path="/" element={< />} /> */}
+    </>
+  )
+}
+
+export default App
